42b8edaaa4f41331468d5690894d70a94d09ebc7
Fotoz <fotozxx@gmail.com>
2017-07-05 11:50:20 +0300

    services+js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(function() {
-
+    
     $('.products-carousel').owlCarousel({
         loop: true,
         smartSpeed: 700,
@@ -16,5 +16,26 @@ $(function() {
             }
         }
     })
+    
+    $(function() {
+        $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+        $('#toTop').fadeIn();
+        } else {
+        $('#toTop').fadeOut();
+        }
+        });
+        $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0},900);
+        });
+    });
+    
+    var $page = $('html, body');
+        $('a[href*="#"]').click(function() {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 900);
+            return false;
+    });
     
 });
